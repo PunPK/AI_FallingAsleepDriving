9a3a5c5932885b24bc8b871a08e9ed2b2f488562
PunPK <phurutsakorn.kps@gmail.com>
2023-06-09 21:30:28 +0700

    AI_FallingAsleepDriving

--- AI_FallingAsleepDriving.py
+++ AI_FallingAsleepDriving.py
@@ -29,10 +29,10 @@ def conv_and_res(ni, nf): return nn.Sequential(conv2(ni, nf), ResBlock(nf))
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
-learn_inf_eye = load_learner('2eye_ModelsfromScratch.pkl')
+learn_inf_eye = load_learner('Model\Teye_ModelsfromScratch.pkl')
 #learn_inf_eye = load_learner('eye_data_resnet18_fastai.pkl')
 #learn_inf_yawn = load_learner('yawn_data_resnet18_fastai.pkl')
-learn_inf_yawn = load_learner('yawn_ModelsfromScratch.pkl')
+learn_inf_yawn = load_learner('Model\yawn_ModelsfromScratch.pkl')
 
 # Variables 
 frame_counter = 0
