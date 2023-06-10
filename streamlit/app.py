c3d00a63f310d0ed612d0c47624f1eb976e188ef
PunPK <phurutsakorn.kps@gmail.com>
2023-06-10 23:03:24 +0700

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import mediapipe as mp
+import numpy as np
 
-st.write("HELLO")
+# Create a MediaPipe Hands instance
+mp_hands = mp.solutions.hands.Hands()
+
+# Use Streamlit to create a selectbox for choosing the video source
+video_source = st.selectbox(
+    "Select video source",
+    options=["Webcam", "Video File (.mp4)"]
+)
